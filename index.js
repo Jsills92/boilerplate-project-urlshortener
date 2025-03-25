3736c9c731cea6e3b13ee67c014514b660b2eafe
Jacob Sills <jsills2592@gmail.com>
2025-03-25 17:29:56 +0000

    Initial Commit

--- index.js
+++ index.js
@@ -19,6 +19,23 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
+app.get("/api/whoami", (req, res) => {
+  
+  // Get the user's IP address (from the 'x-forwarded-for' header or the default 'req.ip' if behind a proxy)
+  let ipaddress = req.headers['x-forwarded-for'] || req.ip;
+  // Get the user's preferred language (Accept-Language header)
+  let language = req.headers['accept-language'].split(',')[0]; // Just the first language in the list
+  // Get the user's software (User-Agent header)
+  let software = req.headers['user-agent'];
+
+  // Respond with JSON object including ipaddress, language, and software
+  res.json({
+    ipaddress: ipaddress,
+    language: language,
+    software: software
+  });
+});
+
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
